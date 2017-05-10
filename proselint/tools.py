997cc8bbc9e9c30f516cd6c0d8a8f32edb409fc3
mcarthurgill <mcarthur.gill@gmail.com>
2017-05-10 16:49:52 -0500

    test

--- proselint/tools.py
+++ proselint/tools.py
@@ -88,8 +88,7 @@ def memoize(f):
 
     # Create the cache if it does not already exist.
     if not os.path.isdir(cache_dirname):
-        os.chmod(cache_dirname, 0o0777)
-        os.makedirs(cache_dirname, 0o0777)
+        os.makedirs(cache_dirname, 0o0777, exist_ok=True)
 
     cache_filename = f.__module__ + "." + f.__name__
     cachepath = os.path.join(cache_dirname, cache_filename)
